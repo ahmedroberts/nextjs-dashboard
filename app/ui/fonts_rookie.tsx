f9567c4a5f9f50894fb34db7235dc79d2baf87e7
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-03-05 14:56:29 -0500

    add apogee wealth logo

--- app/ui/fonts_rookie.tsx
+++ app/ui/fonts_rookie.tsx
@@ -1,25 +1,24 @@
 import { Inter, Lusitana, Emilys_Candy } from 'next/font/google';
 // https://fonts.google.com/
 
-const inter = Inter({
+export const inter = Inter({
     subsets: ['latin'],
     display: 'swap'
 });
 
-const lusitana = Lusitana({
-    weight: ['400', '700'],
+export const lusitana = Lusitana({
+    weight: '400',
     style: 'normal',
     display: 'swap',
     variable: '--font-lusitana',
     subsets: ['latin']
 });
 
-const emilysCandy = Emilys_Candy({
+export const emilysCandy = Emilys_Candy({
     weight: '400',
     style: 'normal',
     display: 'swap',
     variable: '--font-emilysCandy',
     subsets: ['latin']
-});
 
-export { inter, lusitana, emilysCandy };
+});
