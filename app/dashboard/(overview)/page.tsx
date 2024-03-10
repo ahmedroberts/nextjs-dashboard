06648ce1b729b00f1781aa4465ffdf837eedcac3
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-03-10 19:11:22 -0400

    Update finished streaming section

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,19 +1,16 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import LatestInvoicesAOR from '../../ui/dashboard/latest-invoices-aor';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
 
   return (
     <main>
@@ -21,25 +18,22 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton/>}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} /> 
+        {/* <RevenueChart revenue={revenue} /> */}
+        <Suspense fallback={<RevenueChartSkeleton />} >
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
         {/* <LatestInvoices latestInvoices={latestInvoices} /> 
         This one had mapping issue so I created the static version 2024-03-10 
         I did not use the 'await' keyword */}
       </div>
-      <p className="my-2 p-3 bg-ahmedColorScheme01-cambridge-blue 
-        text-ahmedColorScheme01-magnolia rounded-xl">
-          Latest Invoices length = {`${(await latestInvoices).length} `}</p>
       <h3 className="my-2 p-3 bg-ahmedColorScheme01-jade 
         text-ahmedColorScheme01-magnolia rounded-xl">
           Ahmed is the 9th Raikage! 🐂🐙</h3>
