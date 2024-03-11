df18be62dda83875f50b4b8c7c47d0f59b54968f
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-03-11 17:59:41 -0400

    Update lower timeout 90%

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -10,7 +10,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
-
   const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: number | string) => {
