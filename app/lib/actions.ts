06d2316d434d92c378668f0675b0b7b9180f4c23
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-03-12 14:45:21 -0400

    Add update and delete functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,19 +13,9 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+// Use Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
-/* 
-export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    };
-    // Test it out
-    console.log(rawFormData);
-}
-*/
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -49,3 +39,40 @@ export async function createInvoice(formData: FormData) {
     // Now redirect the site to where we want to go
     redirect('/dashboard/invoices')
 }
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+// Delete an Invoice
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
+  
+/* 
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    // Test it out
+    console.log(rawFormData);
+}
+*/
